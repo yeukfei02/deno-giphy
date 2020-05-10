dac2e1c5ccb11a5d8cb53cebfdd26204601be40b
Donald Wu <yeukfei02@gmail.com>
2020-05-10 16:58:28 +0800

    feature/create-project-structure-#1: change route name

--- src/routes/giphy.ts
+++ src/routes/giphy.ts
@@ -11,6 +11,6 @@ const router = new Router();
 router.get("/api/giphy/search-gif", searchGif);
 router.get("/api/giphy/get-trending-gif", getTrendingGif);
 router.get("/api/giphy/get-random-gif", getRandomGif);
-router.get("/api/giphy/:id", getGifById);
+router.get("/api/giphy/gif/:id", getGifById);
 
 export default router;

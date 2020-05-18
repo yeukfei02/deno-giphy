025268db37ceda16eb27e3b90d30c77baa2b1fee
Donald Wu <yeukfei02@gmail.com>
2020-05-18 09:36:11 +0800

    fix-code: update docker-compose yml and db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,9 @@ import { init, MongoClient } from "https://deno.land/x/mongo@v0.6.0/mod.ts";
 await init();
 
 const client = new MongoClient();
-client.connectWithUri("mongodb://localhost:27017");
+const localDbUri = `mongodb://localhost:27017`;
+const dockerDbUri = `mongodb://mongo:27017`;
+client.connectWithUri(localDbUri);
 
 const db = client.database("deno-giphy");
 
